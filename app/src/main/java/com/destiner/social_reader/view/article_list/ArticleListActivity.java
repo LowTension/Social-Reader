59d8c9d90721449ab3a2906c255b0c741ee00dda
Timur Badretdinov <destinerx@gmail.com>
2016-02-23 12:06:28 +0300

    Implement ArticleListView in corresponding Activity

--- app/src/main/java/com/destiner/social_reader/view/article_list/ArticleListActivity.java
+++ app/src/main/java/com/destiner/social_reader/view/article_list/ArticleListActivity.java
@@ -1,15 +1,21 @@
 package com.destiner.social_reader.view.article_list;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.destiner.social_reader.R;
 
-public class ArticleListActivity extends AppCompatActivity {
+public class ArticleListActivity extends AppCompatActivity implements ArticleListView {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
+    }
+
+    @Override
+    public Context getContext() {
+        return this;
     }
 }
