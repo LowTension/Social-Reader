cbb177d38128cb34b6f97822936ad7fd38418656
Timur Badretdinov <destinerx@gmail.com>
2016-02-23 11:41:33 +0300

    Add ArticleListActivity
    This Activity will serve as main, showing most recent articles available.

---
+++ app/src/main/java/com/destiner/social_reader/view/article_list/ArticleListActivity.java
@@ -0,0 +1,15 @@
+package com.destiner.social_reader.view.article_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.destiner.social_reader.R;
+
+public class ArticleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_list);
+    }
+}
