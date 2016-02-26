393cd7ee8217560cc12954fcbd032be7fa1d0195
Timur Badretdinov <destinerx@gmail.com>
2016-02-26 13:10:17 +0300

    Implement Explorer class
    This class loads new posts from different sources.

---
+++ app/src/main/java/com/destiner/social_reader/model/explorer/Explorer.java
@@ -0,0 +1,26 @@
+package com.destiner.social_reader.model.explorer;
+
+import com.destiner.social_reader.model.cache.OnOffsetArticlesLoadListener;
+import com.destiner.social_reader.model.source_manager.SourceManager;
+
+/**
+ * Loads new posts from different sources.
+ */
+public class Explorer {
+
+    /**
+     * Private constructor to forbid class instantiation
+     */
+    private Explorer() {
+    }
+
+    /**
+     * Loads posts
+     * @param length minimal number of posts should be loaded
+     * @param callback callback listener will fire when request will complete
+     */
+    public static void getNew(int length, OnOffsetArticlesLoadListener callback) {
+        // TODO use length parameter to load at least 'length' posts
+        SourceManager.getGroupPosts(callback);
+    }
+}
