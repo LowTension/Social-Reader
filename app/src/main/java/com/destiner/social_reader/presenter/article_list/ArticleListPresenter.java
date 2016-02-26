ab63d49321088fddf6fbc582ca875470a0c9864d
Timur Badretdinov <destinerx@gmail.com>
2016-02-26 13:37:55 +0300

    Load articles from ArticleListPresenter

--- app/src/main/java/com/destiner/social_reader/presenter/article_list/ArticleListPresenter.java
+++ app/src/main/java/com/destiner/social_reader/presenter/article_list/ArticleListPresenter.java
@@ -1,12 +1,24 @@
 package com.destiner.social_reader.presenter.article_list;
 
 
+import com.destiner.social_reader.model.cache.CacheManager;
+import com.destiner.social_reader.model.structs.Post;
 import com.destiner.social_reader.presenter.Presenter;
 import com.destiner.social_reader.view.article_list.ArticleListView;
 
-public class ArticleListPresenter implements Presenter<ArticleListView> {
+import java.util.List;
 
+public class ArticleListPresenter implements Presenter<ArticleListView>, OnArticlesLoadListener {
     private ArticleListView view;
+
+    /**
+     * Loads articles from cache. When ready, callback is fired.
+     * @param count count of posts required
+     * @param offset offset in post list
+     */
+    public void loadArticles(int count, int offset) {
+        CacheManager.getFromCache(count, offset, this);
+    }
 
     @Override
     public void attachView(ArticleListView view) {
@@ -16,5 +28,9 @@ public class ArticleListPresenter implements Presenter<ArticleListView> {
     @Override
     public void detachView() {
         this.view = null;
+    }
+
+    @Override
+    public void onLoad(List<Post> posts) {
     }
 }
