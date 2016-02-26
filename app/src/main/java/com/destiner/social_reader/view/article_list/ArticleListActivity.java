58e8671eda61518de5e47e2a9afab44cee9dc345
Timur Badretdinov <destinerx@gmail.com>
2016-02-26 14:02:30 +0300

    Add Presenter to the ArticleListActivity
    ArticleListPresenter serves as Presenter component

--- app/src/main/java/com/destiner/social_reader/view/article_list/ArticleListActivity.java
+++ app/src/main/java/com/destiner/social_reader/view/article_list/ArticleListActivity.java
@@ -8,12 +8,15 @@ import android.support.v7.widget.RecyclerView;
 
 import com.destiner.social_reader.R;
 import com.destiner.social_reader.model.structs.Post;
+import com.destiner.social_reader.presenter.article_list.ArticleListPresenter;
 
 import java.util.ArrayList;
 
 public class ArticleListActivity extends AppCompatActivity implements ArticleListView {
     RecyclerView articleListRecyclerView;
     ArticleListAdapter adapter;
+
+    ArticleListPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,15 @@ public class ArticleListActivity extends AppCompatActivity implements ArticleLis
         articleListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new ArticleListAdapter(new ArrayList<Post>());
         articleListRecyclerView.setAdapter(adapter);
+
+        presenter = new ArticleListPresenter();
+        presenter.attachView(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        presenter.detachView();
     }
 
     @Override
