68927f2c07321e539f7983ac0e0d076ee1334fec
Timur Badretdinov <destinerx@gmail.com>
2016-02-26 13:54:46 +0300

    Add RecyclerView to ArticleListActivity
    This RecyclerView shows articles, one article per CardView.
    
    * RecyclerView added to Activity layout

--- app/src/main/java/com/destiner/social_reader/view/article_list/ArticleListActivity.java
+++ app/src/main/java/com/destiner/social_reader/view/article_list/ArticleListActivity.java
@@ -1,17 +1,29 @@
 package com.destiner.social_reader.view.article_list;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.destiner.social_reader.R;
+import com.destiner.social_reader.model.structs.Post;
+
+import java.util.ArrayList;
 
 public class ArticleListActivity extends AppCompatActivity implements ArticleListView {
+    RecyclerView articleListRecyclerView;
+    ArticleListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
+
+        articleListRecyclerView = (RecyclerView) findViewById(R.id.article_list_recycler_view);
+        articleListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ArticleListAdapter(new ArrayList<Post>());
+        articleListRecyclerView.setAdapter(adapter);
     }
 
     @Override
