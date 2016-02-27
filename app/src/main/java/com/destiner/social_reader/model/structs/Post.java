e671276c779c8dd6cd9e2ff9502cc2d351131fc5
Timur Badretdinov <destinerx@gmail.com>
2016-02-27 09:57:28 +0300

    Make Post class Serializable

--- app/src/main/java/com/destiner/social_reader/model/structs/Post.java
+++ app/src/main/java/com/destiner/social_reader/model/structs/Post.java
@@ -1,10 +1,12 @@
 package com.destiner.social_reader.model.structs;
 
+import java.io.Serializable;
+
 /**
  * Post structure. Contains information got from API request.
  * Content will be displayed to user.
  */
-public class Post {
+public class Post implements Serializable {
     String text;
 
     public Post(String text) {
