e3fa9e6dd5826a6a90452f4494093999787a177f
Timur Badretdinov <destinerx@gmail.com>
2016-03-01 20:52:21 +0300

    Implement FilterManager class
    This class uses filters to get articles from posts.

---
+++ app/src/main/java/com/destiner/social_reader/model/filter/FilterManager.java
@@ -0,0 +1,50 @@
+package com.destiner.social_reader.model.filter;
+
+import android.content.Context;
+
+import com.destiner.social_reader.model.cache.OnOffsetArticlesLoadListener;
+import com.destiner.social_reader.model.structs.Article;
+import com.destiner.social_reader.model.structs.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Uses filters to get articles from posts. Applies each filter one by one. Every post that passes
+ * becomes an article.
+ */
+public class FilterManager {
+    private static Context context;
+
+    private static List<Filter> filters = new ArrayList<>();
+
+    /**
+     * Applies filters. Fire callback when articles are ready.
+     * @param posts list of posts
+     * @param callback callback listener
+     */
+    public static void filter(List<Post> posts, OnOffsetArticlesLoadListener callback) {
+        // Applies filters
+        for (Filter filter : filters) {
+            posts = filter.filter(posts);
+        }
+        // Creates article instances based on posts text
+        List<Article> articles = new ArrayList<>();
+        for (Post filteredPost : posts) {
+            articles.add(new Article(filteredPost.getText()));
+        }
+        // Fires callback
+        callback.onLoad(articles);
+    }
+
+    public static void setContext(Context c) {
+        context = c;
+        registerFilters();
+    }
+
+    private static void registerFilters() {
+        filters.add(new LengthFilter());
+        filters.add(new FilthFilter(context));
+        filters.add(new InnerLinksFilter());
+    }
+}
