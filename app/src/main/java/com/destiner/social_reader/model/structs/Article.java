2b80c8bec50acafcc10ed6b170a7fd30caf60a73
Timur Badretdinov <destinerx@gmail.com>
2016-03-01 21:04:27 +0300

    Implement Article class
    This class describes post that worth to read.

---
+++ app/src/main/java/com/destiner/social_reader/model/structs/Article.java
@@ -0,0 +1,10 @@
+package com.destiner.social_reader.model.structs;
+
+/**
+ * Post that worth to read. Matches very closely to 'article' dictionary definition.
+ */
+public class Article extends Post {
+    public Article(String text) {
+        super(text);
+    }
+}
