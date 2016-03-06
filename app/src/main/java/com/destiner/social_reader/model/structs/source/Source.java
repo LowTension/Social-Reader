c490faee89555b02f1f14fd35df836a4705e894d
Timur Badretdinov <destinerx@gmail.com>
2016-03-06 12:33:50 +0300

    Remove redundant fields in Source
    With brand new source database™ it is absolutely unnecessary to keep this
    information in the class itself.

--- app/src/main/java/com/destiner/social_reader/model/structs/source/Source.java
+++ app/src/main/java/com/destiner/social_reader/model/structs/source/Source.java
@@ -1,26 +1,10 @@
 package com.destiner.social_reader.model.structs.source;
 
-import org.joda.time.DateTime;
-
 /**
  * Structure that describes where the information come. Every post has its original source.
  */
 public abstract class Source {
-    private static final long maxUnixTime = Integer.MAX_VALUE;
-    private static final long maxUnixTimeMillis = maxUnixTime * 1000;
-
-    private DateTime firstPostTime;
-
     public Source() {
-        firstPostTime = new DateTime(maxUnixTimeMillis);
-    }
-
-    public DateTime getFirstPostTime() {
-        return firstPostTime;
-    }
-
-    public void setFirstPostTime(DateTime firstPostTime) {
-        this.firstPostTime = firstPostTime;
     }
 
     public abstract String getStringId();
