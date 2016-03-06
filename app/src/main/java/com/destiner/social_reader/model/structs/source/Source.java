b413ef00d1e5690a8aa66b62fc53d78e40ed81fa
Timur Badretdinov <destinerx@gmail.com>
2016-03-06 11:55:26 +0300

    Make constants in Source class static

--- app/src/main/java/com/destiner/social_reader/model/structs/source/Source.java
+++ app/src/main/java/com/destiner/social_reader/model/structs/source/Source.java
@@ -6,8 +6,8 @@ import org.joda.time.DateTime;
  * Structure that describes where the information come. Every post has its original source.
  */
 public abstract class Source {
-    private final long maxUnixTime = Integer.MAX_VALUE;
-    private final long maxUnixTimeMillis = maxUnixTime * 1000;
+    private static final long maxUnixTime = Integer.MAX_VALUE;
+    private static final long maxUnixTimeMillis = maxUnixTime * 1000;
 
     private DateTime firstPostTime;
 
