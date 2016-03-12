c4bc765d15fd6c2fdea5a334a7ee3999ac873ed5
Timur Badretdinov <destinerx@gmail.com>
2016-03-12 15:01:55 +0300

    Fix View is null sometimes when Presenter use it
    Of course, the problem lied in the area of Activity Lifecycle. onCreate() calls
    only once, when the Activity is created, but onStop() called every time
    Activity will showed off: power off, starting another Activity or application.
    All these sets view reference in ArticleListPresenter to null, thus leading to
    NPE. Now the reference is updated every time user enters back to the
    ArticleListActivity. Hovewer, we can remove setting of View from onCreate()
    because it makes Preseter to load posts before onStart() will be called.
    
    Simple, smart and argumented fix for the annoying and "hard-to-reproduce" at
    first bug.

--- app/src/main/java/com/destiner/social_reader/view/article_list/ArticleListActivity.java
+++ app/src/main/java/com/destiner/social_reader/view/article_list/ArticleListActivity.java
@@ -57,6 +57,12 @@ public class ArticleListActivity extends AppCompatActivity implements ArticleLis
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        presenter.attachView(this);
+    }
+
+    @Override
     protected void onStop() {
         super.onStop();
         presenter.detachView();
