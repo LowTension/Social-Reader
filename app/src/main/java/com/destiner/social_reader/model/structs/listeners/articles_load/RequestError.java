ddc764e09605e489f08f521ba52956b624e84530
Timur Badretdinov <destinerx@gmail.com>
2016-03-14 21:12:35 +0300

    Add getError() to RequestError

--- app/src/main/java/com/destiner/social_reader/model/structs/listeners/articles_load/RequestError.java
+++ app/src/main/java/com/destiner/social_reader/model/structs/listeners/articles_load/RequestError.java
@@ -1,5 +1,7 @@
 package com.destiner.social_reader.model.structs.listeners.articles_load;
 
+import com.vk.sdk.api.VKError;
+
 public class RequestError {
     public enum Code {
         NO_CONNECTION,
@@ -15,4 +17,17 @@ public class RequestError {
     public Code getCode() {
         return code;
     }
+
+    /**
+     * An adapter from VKError to the inner Error model.
+     * @param errorCode VKError code
+     * @return adapted error
+     */
+    public static RequestError getError(int errorCode) {
+        switch (errorCode) {
+            case VKError.VK_REQUEST_HTTP_FAILED:
+                return new RequestError(Code.NO_CONNECTION);
+        }
+        return null;
+    };
 }
