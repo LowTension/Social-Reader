ab20345dce9c99d7577870a2700685d4a6281ade
Timur Badretdinov <destinerx@gmail.com>
2016-03-24 21:04:14 +0300

    Add ArticleViewer interface

---
+++ app/src/main/java/com/destiner/social_reader/view/article_list/ArticleViewer.java
@@ -0,0 +1,7 @@
+package com.destiner.social_reader.view.article_list;
+
+import com.destiner.social_reader.model.structs.Article;
+
+public interface ArticleViewer {
+    void showArticle(Article article);
+}
